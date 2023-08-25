7a4e59dcc4a98d89e9727a1e482dde4075f24eb1
rafaelbiten <rafaelbiten@icloud.com>
2023-08-24 23:10:58 -0400

    [FEAT] Code to hit and miss guesses

--- islands_engine/.iex.exs
+++ islands_engine/.iex.exs
@@ -1,1 +1,6 @@
 alias IslandsEngine.Impl.{Coordinate, Guesses}
+
+{:ok, coord1} = Coordinate.new(col: 1, row: 1)
+{:ok, coord2} = Coordinate.new(col: 2, row: 2)
+
+guesses = Guesses.new()
--- islands_engine/lib/islands_engine/impl/guesses.ex
+++ islands_engine/lib/islands_engine/impl/guesses.ex
@@ -1,9 +1,16 @@
 defmodule IslandsEngine.Impl.Guesses do
   @moduledoc false
   alias __MODULE__
+  alias IslandsEngine.Impl.Coordinate
 
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
 
   def new, do: %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+
+  def hit(%Guesses{} = guesses, %Coordinate{} = coordinate),
+    do: update_in(guesses.hits, fn hits -> MapSet.put(hits, coordinate) end)
+
+  def miss(%Guesses{} = guesses, %Coordinate{} = coordinate),
+    do: update_in(guesses.misses, fn misses -> MapSet.put(misses, coordinate) end)
 end
