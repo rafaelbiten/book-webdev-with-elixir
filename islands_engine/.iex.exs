4d7c289fdbd9483e57917703055a509bf34864da
rafaelbiten <rafaelbiten@icloud.com>
2023-08-29 21:41:55 -0400

    [CHORE] Checking the game rules on iex

--- islands_engine/.iex.exs
+++ islands_engine/.iex.exs
@@ -1,4 +1,9 @@
+alias IslandsEngine.Rules
 alias IslandsEngine.Impl.{Board, Coordinate, Guesses, Island}
+
+IO.puts(~s"
+# Core components and quick checks
+")
 
 {:ok, coord1} = Coordinate.new(col: 1, row: 1)
 {:ok, coord2} = Coordinate.new(col: 2, row: 2)
@@ -20,7 +25,9 @@ Island.guess(island_dot, coord1)
 |> then(fn {:hit, island_dot} -> Island.forested?(island_dot) end)
 |> IO.inspect(label: "island_dot is forested now?")
 
-guesses = Guesses.new()
+IO.puts(~s"
+# Playing the game with the core modules
+")
 
 game = Board.new()
 |> tap(&IO.inspect(&1, label: "New board"))
@@ -32,3 +39,26 @@ game = Board.new()
 |> tap(&IO.inspect(&1, label: "Guessed coord 1"))
 # |> then(fn {:ok, board} -> Board.guess(board, coord2) end)
 # |> tap(&IO.inspect(&1, label: "Guessed coord 2"))
+
+IO.puts(~s"
+# Game rules
+")
+
+Rules.new()
+|> tap(&IO.inspect(&1, label: "Rules initial state"))
+|> then(fn rules -> Rules.check(rules, :set_player) end)
+|> tap(&IO.inspect(&1, label: "Rules after setting players"))
+|> then(fn {:ok, rules} -> Rules.check(rules, {:position_islands, :p1}) end)
+|> then(fn {:ok, rules} -> Rules.check(rules, {:position_islands, :p2}) end)
+|> then(fn {:ok, rules} -> Rules.check(rules, {:position_islands, :p1}) end)
+|> tap(&IO.inspect(&1, label: "Rules let players position islands freely before locking them"))
+|> then(fn {:ok, rules} -> Rules.check(rules, {:set_islands, :p1}) end)
+|> tap(&IO.inspect(&1, label: "Rules locks player 1 islands"))
+|> tap(fn {:ok, rules} ->
+    IO.puts("Rules :error and won't let player 1 position its islands anymore")
+    :error = Rules.check(rules, {:position_islands, :p1})
+  end)
+|> then(fn {:ok, rules} -> Rules.check(rules, {:position_islands, :p2}) end)
+|> tap(&IO.inspect(&1, label: "Rules still lets player 2 position its islands"))
+|> then(fn {:ok, rules} -> Rules.check(rules, {:set_islands, :p2}) end)
+|> tap(&IO.inspect(&1, label: "Rules locks player 2 islands and starts the game"))
