b0e880060a6834cfac0b1f1bbb034321bd6b5055
rafaelbiten <rafaelbiten@icloud.com>
2023-09-13 21:05:26 -0400

    [FEAT] New game and handling add_player

--- islands_engine/.iex.exs
+++ islands_engine/.iex.exs
@@ -6,3 +6,11 @@ alias IslandsEngine.Impl.Island
 alias IslandsEngine.Rules
 
 # IslandsEngine.Iex.InitialChecks.run()
+
+{:ok, game} = Game.start_link("Raf")
+game_state = :sys.get_state(game)
+
+Game.add_player(game, "Fau")
+
+# Process.info(game)
+# IO.inspect(game_state.player1.name, label: "Player1")
---
+++ islands_engine/lib/islands_engine/game.ex
@@ -0,0 +1,46 @@
+defmodule IslandsEngine.Game do
+  @moduledoc false
+  use GenServer
+
+  alias IslandsEngine.Impl.Board
+  alias IslandsEngine.Impl.Guesses
+  alias IslandsEngine.Rules
+
+  # client
+
+  def start_link(name) when is_binary(name) do
+    GenServer.start_link(__MODULE__, name, [])
+  end
+
+  def init(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
+    new_game = %{player1: player1, player2: player2, rules: %Rules{}}
+    {:ok, new_game}
+  end
+
+  # 2 aliases to get the game state
+  defdelegate get_state(game), to: :sys
+  defdelegate state(state), to: :sys, as: :get_state
+
+  def add_player(game, name) when is_binary(name) do
+    GenServer.call(game, {:add_player, name})
+  end
+
+  # server
+
+  def handle_call({:add_player, name}, _from, game_state) do
+    with {:ok, rules} <- Rules.check(game_state.rules, :set_player) do
+      game_state
+      |> put_in([:player2, :name], name)
+      |> put_in([:rules], rules)
+      |> reply(:ok)
+    else
+      :error -> reply(game_state, :error)
+    end
+  end
+
+  # internals / implementation details
+
+  defp reply(updated_game_state, reply), do: {:reply, reply, updated_game_state}
+end
