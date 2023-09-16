5d997e0d06322d022175eef14eacb4f227f2fa7e
rafaelbiten <rafaelbiten@icloud.com>
2023-09-15 21:57:25 -0400

    [FEAT] Positioning islands

--- islands_engine/lib/islands_engine/game.ex
+++ islands_engine/lib/islands_engine/game.ex
@@ -3,8 +3,12 @@ defmodule IslandsEngine.Game do
   use GenServer
 
   alias IslandsEngine.Impl.Board
+  alias IslandsEngine.Impl.Coordinate
   alias IslandsEngine.Impl.Guesses
+  alias IslandsEngine.Impl.Island
   alias IslandsEngine.Rules
+
+  @players [:p1, :p2]
 
   # client
 
@@ -15,7 +19,7 @@ defmodule IslandsEngine.Game do
   def init(name) do
     player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
     player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
-    new_game = %{player1: player1, player2: player2, rules: %Rules{}}
+    new_game = %{p1: player1, p2: player2, rules: %Rules{}}
     {:ok, new_game}
   end
 
@@ -27,16 +31,36 @@ defmodule IslandsEngine.Game do
     GenServer.call(game, {:add_player, name})
   end
 
+  def position_island(game, player, shape, col, row) when player in @players do
+    GenServer.call(game, {:position_island, player, shape, col, row})
+  end
+
   # server
 
   def handle_call({:add_player, name}, _from, game_state) do
     with {:ok, rules} <- Rules.check(game_state.rules, :add_player) do
       game_state
-      |> put_in([:player2, :name], name)
+      |> put_in([:p2, :name], name)
       |> put_in([:rules], rules)
       |> reply(:ok)
     else
-      :error -> reply(game_state, :error)
+      error -> reply(game_state, error)
+    end
+  end
+
+  def handle_call({:position_island, player, shape, col, row}, _from, game_state) do
+    board = get_in(game_state, [player, :board])
+
+    with {:ok, rules} <- Rules.check(game_state.rules, {:position_island, player}),
+         {:ok, island_position} <- Coordinate.new(col: col, row: row),
+         {:ok, island} <- Island.new(shape, island_position),
+         {:ok, board} <- Board.position_island(board, shape, island) do
+      game_state
+      |> put_in([player, :board], board)
+      |> put_in([:rules], rules)
+      |> reply(:ok)
+    else
+      error -> reply(game_state, error)
     end
   end
 
--- islands_engine/lib/islands_engine/rules.ex
+++ islands_engine/lib/islands_engine/rules.ex
@@ -12,7 +12,7 @@ defmodule IslandsEngine.Rules do
     {:ok, %Rules{rules | state: :players_set}}
   end
 
-  def check(%Rules{state: :players_set} = rules, {:position_islands, player}) do
+  def check(%Rules{state: :players_set} = rules, {:position_island, player}) do
     case Map.fetch!(rules, player) do
       :islands_set -> :error
       :islands_not_set -> {:ok, rules}
