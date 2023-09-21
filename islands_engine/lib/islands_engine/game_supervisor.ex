375460da75c41b7640de3a44fff7129f305f0812
rafaelbiten <rafaelbiten@icloud.com>
2023-09-20 22:23:56 -0400

    [FEAT] Stopping games from the dynamic supervisor

--- islands_engine/lib/islands_engine/game_supervisor.ex
+++ islands_engine/lib/islands_engine/game_supervisor.ex
@@ -6,6 +6,15 @@ defmodule IslandsEngine.GameSupervisor do
     DynamicSupervisor.start_child(__MODULE__, {Game, player_one_name})
   end
 
+  def stop_game(player_one_name) do
+    player_one_name
+    |> find_game_by_name()
+    |> then(fn
+      nil -> {:error, :game_not_found}
+      game_pid -> DynamicSupervisor.terminate_child(__MODULE__, game_pid)
+    end)
+  end
+
   def find_game_by_name(player_one_name) when is_binary(player_one_name) do
     player_one_name
     |> Game.via_tuple()
